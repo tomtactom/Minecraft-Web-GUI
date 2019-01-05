2655f70076caec77843d871e3f21d1aa5b6e28ca
tom12aschmann <32821177+tom12aschmann@users.noreply.github.com>
2019-01-05 22:16:48 +0100

    Domain wurde entfernt

--- src/config.php
+++ src/config.php
@@ -1,6 +1,6 @@
 <?php
 	// Gebe hier die Domain oder IP deines Minecraft-Servers ein
-	$host = 'nettom.ddnss.de';
+	$host = '';
 	$port = 25575;
 	// Gebe hier dein Rcon-Passwort ein
 	$password = '';
