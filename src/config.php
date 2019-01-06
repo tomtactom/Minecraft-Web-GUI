9d4a4b55fcf3fd60437b2a5bb8bb33f7b32f8214
Tom <t.asch2812@gmail.com>
2019-01-06 19:00:43 +0100

    config ausgeklammert

--- src/config.php
+++ src/config.php
@@ -8,6 +8,7 @@
 	$timeout = 5;
 */
 //Trage hier die Verbindungsdaten der Datenbank ein
+/*
 $db_host = 'localhost:3306';
 $db_name = 'mc_login';
 $db_user = '';
@@ -16,6 +17,6 @@ $pageurl = '';
 $showFormular = true;
 $showForm = true;
 $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_password);
-
+*/
 include('./src/config.inc.php');
 include('./config.inc.php');
