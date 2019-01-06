549cfca9a6a27007755d93311314768cb7599b36
Tom <t.asch2812@gmail.com>
2019-01-06 17:28:18 +0100

    Favicons hinzugefügt

--- index.php
+++ index.php
@@ -27,7 +27,24 @@
 		<meta name="description" content="Spiele auf deinem Minecraft Server und schau dir auf dieser Seite die Statistiken dazu an.">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <meta name="msapplication-TileColor" content="#8cbeff">
+        <meta name="msapplication-TileImage" content="./src/favicon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#8cbeff">
 		<title><?php echo $ServerName; ?> GUI</title>
+        <link rel="apple-touch-icon" sizes="57x57" href="./src/favicon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="./src/favicon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="./src/favicon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="./src/favicon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="./src/favicon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="./src/favicon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="./src/favicon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="./src/favicon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="./src/favicon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="./src/favicon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="./src/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="./src/favicon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="./src/favicon/favicon-16x16.png">
+        <link rel="manifest" href="./src/favicon/manifest.json">
 		<link rel="stylesheet" href="./src/layout.css">
 		<link href='https://fonts.googleapis.com/css?family=Sanchez' rel='stylesheet' type='text/css'>
 		<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
