7fd270a24f09e43674e149e488e2108d03243842
Tom <t.asch2812@gmail.com>
2019-01-06 20:13:51 +0100

    Fehler behoben else if

--- src/header.php
+++ src/header.php
@@ -10,7 +10,7 @@
 					<li class="first"><a href=""> Startseite </a> </li>
 					<li><a href="./regeln"> Regeln </a> </li>
 					<li><a href="./hilfe"> Hilfe </a> </li>
-					<?php if(is_checked_in()): ?>
+					<?php if(is_checked_in()) { ?>
 					<li><a href="./logout"> Ausloggen </a></li>
 					<?php 
 						} else {
