1dbf1f77bbf75ca647abfba48ca62068c715c41a
Tom <t.asch2812@gmail.com>
2019-01-08 17:38:59 +0100

    Syntax Fehler behoben

--- src/head.php
+++ src/head.php
@@ -15,7 +15,7 @@
 		$ServerVersion = $Server->Get('version');
 	} else {
 		$ServerName = 'Lan-Party';
-		$ServerVersion = 1.13.2
+		$ServerVersion = "1.13.2";
 	}
     if(is_checked_in()) {
         if(!empty($_POST["command"])) {
