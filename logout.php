120d31d640a7ea06055dc39757b5d30a0a1b1781
Tom <t.asch2812@gmail.com>
2019-01-11 20:47:31 +0100

    Logout fertiggestellt

--- logout.php
+++ logout.php
@@ -5,5 +5,5 @@ session_start();
     if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS']!='off' or $_SERVER['SERVER_PORT']==443) $protocol='https://';
     else $protocol='http://';
 ?>
-<!--<script>location.replace('<?php echo $protocol.$_SERVER["HTTP_HOST"]; ?>');</script>
-<meta http-equiv="refresh" content="0; URL="<?php echo $protocol.$_SERVER["HTTP_HOST"]; ?>">-->
+<script>location.replace('<?php echo $protocol.$_SERVER["HTTP_HOST"]; ?>');</script>
+<meta http-equiv="refresh" content="0; URL="<?php echo $protocol.$_SERVER["HTTP_HOST"]; ?>">
