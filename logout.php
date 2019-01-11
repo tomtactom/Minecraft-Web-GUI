42f475ba3c9c9211fc5fd4b4a6e34721da307691
Tom <t.asch2812@gmail.com>
2019-01-11 20:39:05 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
-echo session_unset()
+echo session_unset();
 	unset($_SESSION['id']);
     if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS']!='off' or $_SERVER['SERVER_PORT']==443) $protocol='https://';
     else $protocol='http://';
