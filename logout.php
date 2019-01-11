13cb921b0b41a800dc457c42dc0350cbf9e7a9fa
Tom <t.asch2812@gmail.com>
2019-01-11 20:35:37 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,13 @@
 <?php
-	session_destroy();
+	if(session_destroy()) {
 	unset($_SESSION['userid']);
     if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS']!='off' or $_SERVER['SERVER_PORT']==443) $protocol='https://';
     else $protocol='http://';
 ?>
 <script>location.replace('<?php echo $protocol.$_SERVER["HTTP_HOST"]; ?>');</script>
 <meta http-equiv="refresh" content="0; URL="<?php echo $protocol.$_SERVER["HTTP_HOST"]; ?>">
+<?php
+    } else {
+        echo "false";
+    }
+?>
