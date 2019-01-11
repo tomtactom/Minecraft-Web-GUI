74f5afe5b1e222007a48323c5e9e3b6e17fdab74
Tom <t.asch2812@gmail.com>
2019-01-11 20:33:42 +0100

    http/https Fehler behoben

--- logout.php
+++ logout.php
@@ -1,6 +1,8 @@
 <?php
 	session_destroy();
 	unset($_SESSION['userid']);
+    if(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS']!='off' or $_SERVER['SERVER_PORT']==443) $protocol='https://';
+    else $protocol='http://';
 ?>
-<script>location.replace('<?php echo $_SERVER["HTTP_HOST"]; ?>');</script>
-<meta http-equiv="refresh" content="0; URL="<?php echo $_SERVER["HTTP_HOST"]; ?>">
+<script>location.replace('<?php echo $protocol.$_SERVER["HTTP_HOST"]; ?>');</script>
+<meta http-equiv="refresh" content="0; URL="<?php echo $protocol.$_SERVER["HTTP_HOST"]; ?>">
