68c6af2a16cd2413d48541f34ea414579d4367ab
Tom <t.asch2812@gmail.com>
2019-01-17 21:43:16 +0100

    css

--- src/header.php
+++ src/header.php
@@ -31,7 +31,6 @@
 			</div>
 		</nav>
 		<header>
-            <div class="announcement_block"></div>
 			<div class="announcement">
 				<ul id="items">
 					<li>Willkommen auf dem <?php echo $ServerName; ?> Server</li>
@@ -41,7 +40,8 @@
 					<li>Starte Minecraft mit der Version <?php echo $ServerVersion; ?></li>
 				</ul>
 			</div>
-            <div class="announcement_block"></div>
+            <div class="announcement_block-l"></div>
+            <div class="announcement_block-r"></div>
 			<div class="triangle-l"></div>
 			<div class="triangle-r"></div>
 		</header>
