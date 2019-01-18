d18675f6c874a3e2bb19c971123faec11f9be710
Tom <t.asch2812@gmail.com>
2019-01-18 22:53:36 +0100

    aside erneuert

--- hilfe.php
+++ hilfe.php
@@ -3,6 +3,21 @@
     require('./src/header.php');
 ?>
 <main>
+    <aside>
+        <section>
+            <h3 class="address">Serveradresse</h3>
+            <p class="address">
+                <?php echo $host; ?>
+            </p>
+        </section>
+        <section>
+            <h3>Server Status: </h3>
+            <div class="online"></div>
+            <span id="serverstatus" class="online"></span>
+            <div class="offline"></div>
+            <p class="offline">Der Server ist momentan Offline</p>
+        </section>
+    </aside>
     <article>
         <p><?php echo $error_msg; ?></p>
         <section>
@@ -152,17 +167,6 @@
             <p>&copy;<?php echo date("Y"); ?> Tom Aschmann</p>
         </footer>
     </article>
-    <aside>
-        <h3 class="address">Serveradresse</h3>
-        <p class="address"><?php echo $host; ?></p>
-    </aside>
-    <aside>
-        <h3>Server Status: </h3>
-        <div class="online"></div>
-        <span id="serverstatus" class="online"></span>
-        <div class="offline"></div>
-        <p class="offline">Der Server ist momentan Offline</p>
-    </aside>
 </main>
 <?php
     require('./src/footer.php');
